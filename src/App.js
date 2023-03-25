a5e6f1655e8c4ca82ec1af9ac6c66bfd2e96e923
Amdrii <andreyk557@gmail.com>
2023-03-25 15:46:56 +0200

    feat[waveShaderMaterial-function]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import { Canvas } from "@react-three/fiber";
-
+import { Canvas, extend } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
 import "./App.css";
+
+const WaveShaderMaterial = shaderMaterial(
+  {},
+  glsl`
+   void main(){
+     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+   }
+   `,
+
+  glsl`
+  void main(){
+    gl_FragColor = vec4(0.0, 0.4,1.0, 1.0);
+  }
+  `
+);
+
+extend({ WaveShaderMaterial });
 
 const Scene = () => {
   return (
@@ -8,7 +26,7 @@ const Scene = () => {
       <pointLight position={[10, 10, 10]} />
       <mesh>
         <planeBufferGeometry args={[3, 5]} />
-        <meshStandardMaterial color="lightblue" />
+        <waveShaderMaterial />
       </mesh>
     </Canvas>
   );
