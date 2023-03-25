e0c1d71658a1d833651b8ceeef5aef16ad5c0d57
Amdrii <andreyk557@gmail.com>
2023-03-25 16:23:26 +0200

    feat[wave]

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import { Canvas, extend } from "@react-three/fiber";
+import * as THREE from "three";
+import { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame } from "@react-three/fiber";
 import { shaderMaterial } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro";
 import "./App.css";
 
 const WaveShaderMaterial = shaderMaterial(
-  {},
+  { uTime: 0, uColor: new THREE.Color(0.0, 0.0, 0.0) },
   glsl`
+  varying vec2 vUv;
    void main(){
+    vUv = uv;
      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
    }
    `,
 
   glsl`
-  void main(){
-    gl_FragColor = vec4(0.0, 0.4,1.0, 1.0);
+  precision mediump float;
+
+   uniform vec3 uColor;
+   uniform float uTime;
+   
+   varying vec2 vUv;
+
+    void main(){
+    gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 1.0);
   }
   `
 );
 
 extend({ WaveShaderMaterial });
 
+const Wave = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  return (
+    <mesh>
+      <planeBufferGeometry args={[3, 5]} />
+      <waveShaderMaterial uColor={"hotpink"} />
+    </mesh>
+  );
+};
+
 const Scene = () => {
   return (
     <Canvas>
-      <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <planeBufferGeometry args={[3, 5]} />
-        <waveShaderMaterial />
-      </mesh>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
     </Canvas>
   );
 };
