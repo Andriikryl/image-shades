8ecb5e4318aa23378facfb8287249ff78bee168d
Amdrii <andreyk557@gmail.com>
2023-03-25 17:02:01 +0200

    feat[shade-configurate]

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 import * as THREE from "three";
-import { useRef, Suspense } from "react";
-import { Canvas, extend, useFrame } from "@react-three/fiber";
+import React, { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
 import { shaderMaterial } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro";
 import "./App.css";
+import Front from "./images/Front.jpg";
 
 const WaveShaderMaterial = shaderMaterial(
-  { uTime: 0, uColor: new THREE.Color(0.0, 0.0, 0.0) },
+  // Uniform
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0.0, 0.0, 0.0),
+    uTexture: new THREE.Texture(),
+  },
+  // Vertex Shader
   glsl`
-  varying vec2 vUv;
-   void main(){
-    vUv = uv;
-     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-   }
-   `,
+    precision mediump float;
 
+    varying vec2 vUv;
+    varying float vWave;
+
+    uniform float uTime;
+
+    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
+
+    void main() {
+      vUv = uv;
+
+      vec3 pos = position;
+      float noiseFreq = 2.0;
+      float noiseAmp = 0.4;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+      vWave = pos.z;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);  
+    }
+  `,
+  // Fragment Shader
   glsl`
-  precision mediump float;
+    precision mediump float;
 
-   uniform vec3 uColor;
-   uniform float uTime;
-   
-   varying vec2 vUv;
+    uniform vec3 uColor;
+    uniform float uTime;
+    uniform sampler2D uTexture;
 
-    void main(){
-    gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 1.0);
-  }
+    varying vec2 vUv;
+    varying float vWave;
+
+    void main() {
+      float wave = vWave * 0.2;
+      vec3 texture = texture2D(uTexture, vUv + wave).rgb;
+      gl_FragColor = vec4(texture, 1.0); 
+    }
   `
 );
 
@@ -35,17 +63,21 @@ const Wave = () => {
   const ref = useRef();
   useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
 
+  const [image] = useLoader(THREE.TextureLoader, [
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e2e475ef-615e-4f8b-a488-697c7bab6d01/dfsl2d1-6eca00d5-25fd-40b7-880c-e21067c451cd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2UyZTQ3NWVmLTYxNWUtNGY4Yi1hNDg4LTY5N2M3YmFiNmQwMVwvZGZzbDJkMS02ZWNhMDBkNS0yNWZkLTQwYjctODgwYy1lMjEwNjdjNDUxY2QucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dEyEIaihDKIC3JQBrqUT3DgI_BtNdqwLuOSI6GLdePI",
+  ]);
+
   return (
     <mesh>
-      <planeBufferGeometry args={[3, 5]} />
-      <waveShaderMaterial uColor={"hotpink"} />
+      <planeBufferGeometry args={[0.8, 0.8, 20, 20]} />
+      <waveShaderMaterial uColor={"black"} ref={ref} uTexture={image} />
     </mesh>
   );
 };
 
 const Scene = () => {
   return (
-    <Canvas>
+    <Canvas camera={{ fov: 12, position: [0, 0, 5] }}>
       <Suspense fallback={null}>
         <Wave />
       </Suspense>
@@ -54,7 +86,11 @@ const Scene = () => {
 };
 
 const App = () => {
-  return <Scene />;
+  return (
+    <>
+      <Scene />
+    </>
+  );
 };
 
 export default App;
