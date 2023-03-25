325ef6b09e09f565ad3e133ebeb60487abfe12ab
Amdrii <andreyk557@gmail.com>
2023-03-25 15:32:30 +0200

    feat[simple-scene]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
 
-function App() {
+import "./App.css";
+
+const Scene = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <pointLight position={[10, 10, 10]} />
+      <mesh>
+        <planeBufferGeometry args={[3, 5]} />
+        <meshStandardMaterial color="lightblue" />
+      </mesh>
+    </Canvas>
   );
-}
+};
+
+const App = () => {
+  return <Scene />;
+};
 
 export default App;
